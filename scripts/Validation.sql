4e1a18c39614e2881eea28a6263bdb10c9deb6f7
Chibuzo Nwankwo <55243830+nchibuzo74@users.noreply.github.com>
2025-12-30 00:30:03 +0100

    File Added

--- scripts/Validation.sql
+++ scripts/Validation.sql
@@ -1,3 +1,24 @@
+--------------------------------------------------------------------------------------------------------------------
+---------------------------bronze_staging---------------------------------------------------------------------------------------
+drop table bronze.crm_sales_details_staging;
+drop table bronze.crm_prd_info_staging;
+drop table bronze.crm_cust_info_staging;
+drop table bronze.erp_loc_a101_staging;
+drop table bronze.erp_cust_az12_staging;
+drop table bronze.erp_px_cat_g1v2_staging;
+
+----------------------------silver layer------------------------------------------------------------------------------------------
+drop table silver.crm_sales_details;
+drop table silver.crm_prd_info;
+drop table silver.crm_cust_info;
+drop table silver.erp_loc_a101;
+drop table silver.erp_cust_az12;
+drop table silver.erp_px_cat_g1v2;
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+
+
+----------------------------Retrieval of data----------------------------------------------------------------------
 SELECT count(*) as total_record 
 from gold.dim_customers
 where country is null;
@@ -17,24 +38,6 @@ select * from bronze.crm_sales_details;
 select * from silver.crm_cust_info;
 select * from silver.crm_prd_info;
 select * from silver.crm_sales_details;
-
-
----------------------------bronze_staging---------------------------------------------------------------------------------------
-drop table bronze.crm_sales_details_staging;
-drop table bronze.crm_prd_info_staging;
-drop table bronze.crm_cust_info_staging;
-drop table bronze.erp_loc_a101_staging;
-drop table bronze.erp_cust_az12_staging;
-drop table bronze.erp_px_cat_g1v2_staging;
-
-----------------------------silver layer------------------------------------------------------------------------------------------
-drop table silver.crm_sales_details;
-drop table silver.crm_prd_info;
-drop table silver.crm_cust_info;
-drop table silver.erp_loc_a101;
-drop table silver.erp_cust_az12;
-drop table silver.erp_px_cat_g1v2;
---------------------------------------------------------------------------------------------------------------------
 
 select * from gold.customers;
 select * from gold.products;
